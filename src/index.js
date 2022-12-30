78df01859f06631b85edebde3a85ee3b8d6aae99
ertyhd <orionship@protonmail.com>
2022-12-30 09:18:20 +0200

    Try to fix parcel

---
+++ src/index.js
@@ -0,0 +1,5 @@
+
+import('./js/teamModal');
+
+import './js/firebaseGoogleAuth';
+
---
+++ src/js/test-fetchest.js
@@ -0,0 +1,60 @@
+import NewApiFetches from './js/apiFetches';
+import * as basicLightbox from 'basiclightbox';
+const newApiFetches = new NewApiFetches();
+
+// ------------ Tranding films for SLIDERS -----
+newApiFetches
+  .fetchTrendingMovie()
+  .then(data => {
+    console.log('Trending ', data);
+  })
+  .catch(error => console.log(error));
+
+// -------------- Popular films for BODY ------
+newApiFetches
+  .fetchPopularMovie()
+  .then(data => {
+    console.log('Popular ', data);
+  })
+  .catch(error => console.log(error));
+
+// ------------- Search films -----------------
+// Запрос fetchSearchMovie(name, page)
+newApiFetches
+  .fetchSearchMovie('max', 1)
+  .then(data => {
+    console.log('Search ', data);
+  })
+  .catch(error => console.log(error));
+
+// ----------- Ditails for MODAL WINDOW -------
+// Запрос newApiFetches fetchDetailsMovie(id)
+newApiFetches
+  .fetchDetailsMovie(76600)
+  .then(data => {
+    console.log('Details ', data);
+  })
+  .catch(error => console.log(error));
+
+// ------------- Trailer ----------------------
+// Запрос newApiFetches fetchTrailerMovie(id)
+const list = document.querySelector('body');
+newApiFetches
+  .fetchTrailerMovie(76341)
+  .then(data => {
+    console.log('Trailer ', data.results);
+    const id = data.results[0].key;
+    const instance = basicLightbox.create(`
+  <iframe width="560" height="315" src='https://www.youtube.com/embed/${id}'frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+`);
+    instance.show();
+  })
+  .catch(error => console.log(error));
+
+// "devDependencies": {
+//   "@parcel/transformer-sass": "^2.6.0",
+//   "parcel": "^2.6.0",
+//   "parcel-reporter-clean-dist": "^1.0.4",
+//   "posthtml-include": "^1.7.4",
+//   "process": "^0.11.10"
+// },
