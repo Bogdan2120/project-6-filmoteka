33db7ecd4a151673e119c3e4ef5bac46caf724cd
Bogdan2120 <bogdanvasilik2120@gmail.com>
2022-12-30 20:31:16 +0000

    Deploying to gh-pages from @ Bogdan2120/project-6-filmoteka@6ff347a72043e5c2cc9a92b9a829de7ff27e06a9 🚀

--- teamModal.a0bf74f1.js
+++
@@ -1,2 +0,0 @@
-("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequired7c6.register("dsadE",(function(e,o){!function(){var e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}()}));
-//# sourceMappingURL=teamModal.a0bf74f1.js.map
