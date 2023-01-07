14ff252ece2448bebb179f84c224e277e5089eb4
DydikN <PingvinPain@gmail.com>
2023-01-07 13:23:56 +0200

    loading and hover
    add loading dots for my library and hover for buttons

--- src/js/my-library.js
+++ src/js/my-library.js
@@ -10,6 +10,7 @@ export const refs = {
   btnQueue: document.querySelector('[name="queue"]'),
   defaultText: document.querySelector('[name="default-text"]'),
   gallery: document.querySelector('.my-library-film-card'),
+  loading: document.querySelector('.spinner-box'),
 };
 
 // render
@@ -17,11 +18,14 @@ let watched = getWatchedFilms();
 let queue = getQueueFilms();
 
 const loadWatchedFilms = () => {
+  refs.btnQueue.classList.remove('btn-header__active');
+  refs.btnWatched.classList.add('btn-header__active');
+
   if (watched === null) {
     return;
   } else {
     clearMarkup();
-
+    refs.loading.classList.remove('is-hidden');
     watched.map(id =>
       newApiFetches
         .fetchMovieById(id)
@@ -29,16 +33,22 @@ const loadWatchedFilms = () => {
           renderMoviesList(data);
         })
         .catch(err => new Error(err))
+        .finally(() => {
+          refs.loading.classList.add('is-hidden');
+        })
     );
     refs.defaultText.classList.add('is-hidden');
   }
 };
 
 const loadQueueFilms = () => {
+  refs.btnQueue.classList.add('btn-header__active');
+  refs.btnWatched.classList.remove('btn-header__active');
   if (queue === null) {
     return;
   } else {
     clearMarkup();
+    refs.loading.classList.remove('is-hidden');
 
     queue.map(id =>
       newApiFetches
@@ -47,6 +57,9 @@ const loadQueueFilms = () => {
           renderMoviesList(data);
         })
         .catch(err => new Error(err))
+        .finally(() => {
+          refs.loading.classList.add('is-hidden');
+        })
     );
     refs.defaultText.classList.add('is-hidden');
   }
